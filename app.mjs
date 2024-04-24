150d2bcc8329bafb13e5ebc18d289ddf0a21bc33
Kosmas Archontis <up1084020@ac.upatras.gr>
2024-04-24 21:06:29 +0300

    Set up server

---
+++ app.mjs
@@ -0,0 +1,34 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+
+const app = express();
+const router = express.Router();
+const port = process.env.PORT || '3000';
+
+app.use(express.static('public'));
+
+app.engine('hbs', engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+
+
+let eventNavigation = function (req, res) {
+    const navigateTo = req.query.navigates;
+    if (navigateTo === 'theater'){
+        res.render('events', { title: 'THEATER' });
+    }
+    else {
+        res.render('events');
+    }
+};
+
+app.use(router);
+
+router.route('/').get((req, res) => {
+    res.render('index');
+});
+
+router.route('/navigation').get(eventNavigation);
+
+const server = app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
