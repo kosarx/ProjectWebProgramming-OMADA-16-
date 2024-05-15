818b8baa4585c3093b383a6bb0edf6f2c454c21a
mariaaspioti <up1083881@ac.upatras.gr>
2024-05-16 00:12:59 +0300

    finished booking except for modal, made booking available to all events and added gradient to event image

--- controllers/routes/events.js
+++ controllers/routes/events.js
@@ -130,10 +130,11 @@ let bookTicketsNavigation = async function (req, res) {
                 res.json({ error: err });
             }
             else {
-
                 eventInfo = eventInfo[0];
                 eventInfo.title = eventInfo.title.toUpperCase();
-                eventInfo.type = "cinema"
+                eventInfo.type = navigateTo;
+                eventInfo.locations = '';
+                eventInfo.dates = '';
                 model.getEventReviews(eventID, (err, reviewList) => {
                     if (err) {
                         console.log("reviews")
@@ -174,8 +175,6 @@ let bookTicketsNavigation = async function (req, res) {
                                     showList[i].show_date = formattedDate;
                                     showList[i].show_day = dayName;
 
-                                    console.log(showList[i].show_date, showList[i].show_day)
-
                                     let timeString = showList[i].show_time;
 
                                     // Split the time string into hours, minutes, and seconds
@@ -186,16 +185,32 @@ let bookTicketsNavigation = async function (req, res) {
                                     hours = (hours % 12) || 12;
 
                                     // Construct the formatted time string
-                                    let formattedTime = hours + ':' + minutes + ampm;
-
-                                    console.log('Formatted Time:', formattedTime);
+                                    let formattedTime = hours + ':' + minutes + ' ' + ampm;
 
                                     showList[i].show_time = formattedTime;
 
+                                    if (showList[i].venue_name == showList[i].address) {
+                                        showList[i].venue_name = null;
+                                    }
 
-                                    // const formattedDate = moment(showList[i].show_date).format('YYYY-MM-DD');
-                                    // console.log(formattedDate)
+                                    if (!eventInfo.locations.includes(showList[i].city)){
+                                        eventInfo.locations = eventInfo.locations + ', ' + showList[i].city;
+                                    }
+                                    if (!eventInfo.dates.includes(showList[i].show_date)){
+                                        eventInfo.dates = eventInfo.dates + ', ' + showList[i].show_date;
+                                    }
                                 }
+
+                                eventInfo.locations = eventInfo.locations.replace(/^, /, ''); // remove ", " from the beginning of the string
+                                eventInfo.dates = eventInfo.dates.replace(/^, /, ''); // remove ", " from the beginning of the string
+                                let titleAndArtist = capitalizeFirstLetter(eventInfo.title) + ' -' + eventInfo.lead_roles;
+                                console.log("before",titleAndArtist)
+                                if(titleAndArtist.length>58) {
+                                    titleAndArtist = titleAndArtist.slice(0,40)+'...';
+                                    console.log("after",titleAndArtist)
+                                }
+
+                                eventInfo.titleAndArtist = titleAndArtist;
 
                                 res.render('temp', { eventInfo, reviewList, showList });
                             }
@@ -209,67 +224,224 @@ let bookTicketsNavigation = async function (req, res) {
 
     }
     else if (navigateTo === 'music') {
-        model.getMusicEventInfo(eventID, (err, data) => {
+        model.getMusicEventInfo(eventID, (err, eventInfo) => {
             if (err) {
                 console.log("event")
                 res.json({ error: err });
             }
             else {
-                eventInfo = data;
-                // res.send(eventInfo)
+                eventInfo = eventInfo[0];
+                eventInfo.title = eventInfo.title.toUpperCase();
+                eventInfo.type = navigateTo;
+                eventInfo.locations = '';
+                eventInfo.dates = '';
+                model.getEventReviews(eventID, (err, reviewList) => {
+                    if (err) {
+                        console.log("reviews")
+                        res.json({ error: err });
+                    }
+                    else {
+                        reviewList.forEach(review => {
+                            review.username = '@' + review.username;
+                            review.username = review.username.replace(/['\s]/g, '');
+                        });
+
+                        model.getShowInfo(eventID, (err, showList) => {
+                            if (err) {
+                                console.log("show")
+
+                                res.json({ error: err });
+                            }
+                            else {
+                                const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+                                const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                                let formattedDate;
+                                let dayName;
+                                let finalFormattedDate;
+                                let showDate;
+                                for (let i in showList) {
+                                    showList[i].minimum_price = parseFloat(showList[i].minimum_price).toFixed(2);
+
+                                    // Format the date components
+
+                                    showDate = new Date(showList[i].show_date);
+
+                                    // Format the date components
+                                    formattedDate = showDate.getDate().toString().padStart(2, '0') + '-' + (showDate.getMonth() + 1).toString().padStart(2, '0') + '-' + showDate.getFullYear().toString();
+
+                                    // Get the day name
+                                    dayName = dayNames[showDate.getDay()];
+
+                                    showList[i].show_date = formattedDate;
+                                    showList[i].show_day = dayName;
+
+                                    let timeString = showList[i].show_time;
+
+                                    // Split the time string into hours, minutes, and seconds
+                                    let [hours, minutes, seconds] = timeString.split(':');
+
+                                    // Convert the hours to a 12-hour format with AM/PM indication
+                                    let ampm = hours >= 12 ? 'pm' : 'am';
+                                    hours = (hours % 12) || 12;
+
+                                    // Construct the formatted time string
+                                    let formattedTime = hours + ':' + minutes + ' ' + ampm;
+
+                                    showList[i].show_time = formattedTime;
+
+                                    if (showList[i].venue_name == showList[i].address) {
+                                        showList[i].venue_name = null;
+                                    }
+
+                                    if (!eventInfo.locations.includes(showList[i].city)){
+                                        eventInfo.locations = eventInfo.locations + ', ' + showList[i].city;
+                                    }
+                                    if (!eventInfo.dates.includes(showList[i].show_date)){
+                                        eventInfo.dates = eventInfo.dates + ', ' + showList[i].show_date;
+                                    }
+                                }
+
+                                eventInfo.locations = eventInfo.locations.replace(/^, /, ''); // remove ", " from the beginning of the string
+                                eventInfo.dates = eventInfo.dates.replace(/^, /, ''); // remove ", " from the beginning of the string
+                                let titleAndArtist = capitalizeFirstLetter(eventInfo.title) + ' -' + eventInfo.artists;
+                                console.log("before",titleAndArtist)
+                                if(titleAndArtist.length>58) {
+                                    titleAndArtist = titleAndArtist.slice(0,40)+'...';
+                                    console.log("after",titleAndArtist)
+                                }
+
+                                eventInfo.titleAndArtist = titleAndArtist;
+
+                                res.render('temp', { eventInfo, reviewList, showList });
+                            }
+                        })
+
+                    }
+                })
             }
 
         });
 
     }
     else if (navigateTo === 'theater') {
-        model.getTheaterEventInfo(eventID, (err, data) => {
+        model.getTheaterEventInfo(eventID, (err, eventInfo) => {
             if (err) {
                 console.log("event")
                 res.json({ error: err });
             }
             else {
-                eventInfo = data;
-                // res.send(eventInfo)
+                eventInfo = eventInfo[0];
+                eventInfo.title = eventInfo.title.toUpperCase();
+                eventInfo.type = navigateTo;
+                eventInfo.locations = '';
+                eventInfo.dates = '';
+                model.getEventReviews(eventID, (err, reviewList) => {
+                    if (err) {
+                        console.log("reviews")
+                        res.json({ error: err });
+                    }
+                    else {
+                        reviewList.forEach(review => {
+                            review.username = '@' + review.username;
+                            review.username = review.username.replace(/['\s]/g, '');
+                        });
+
+                        model.getShowInfo(eventID, (err, showList) => {
+                            if (err) {
+                                console.log("show")
+
+                                res.json({ error: err });
+                            }
+                            else {
+                                const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+                                const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                                let formattedDate;
+                                let dayName;
+                                let finalFormattedDate;
+                                let showDate;
+                                for (let i in showList) {
+                                    showList[i].minimum_price = parseFloat(showList[i].minimum_price).toFixed(2);
+
+                                    // Format the date components
+
+                                    showDate = new Date(showList[i].show_date);
+
+                                    // Format the date components
+                                    formattedDate = showDate.getDate().toString().padStart(2, '0') + '-' + (showDate.getMonth() + 1).toString().padStart(2, '0') + '-' + showDate.getFullYear().toString();
+
+                                    // Get the day name
+                                    dayName = dayNames[showDate.getDay()];
+
+                                    showList[i].show_date = formattedDate;
+                                    showList[i].show_day = dayName;
+
+                                    let timeString = showList[i].show_time;
+
+                                    // Split the time string into hours, minutes, and seconds
+                                    let [hours, minutes, seconds] = timeString.split(':');
+
+                                    // Convert the hours to a 12-hour format with AM/PM indication
+                                    let ampm = hours >= 12 ? 'pm' : 'am';
+                                    hours = (hours % 12) || 12;
+
+                                    // Construct the formatted time string
+                                    let formattedTime = hours + ':' + minutes + ' ' + ampm;
+
+                                    showList[i].show_time = formattedTime;
+
+                                    if (showList[i].venue_name == showList[i].address) {
+                                        showList[i].venue_name = null;
+                                    }
+
+                                    if (!eventInfo.locations.includes(showList[i].city)){
+                                        eventInfo.locations = eventInfo.locations + ', ' + showList[i].city;
+                                    }
+                                    if (!eventInfo.dates.includes(showList[i].show_date)){
+                                        eventInfo.dates = eventInfo.dates + ', ' + showList[i].show_date;
+                                    }
+                                }
+
+                                eventInfo.locations = eventInfo.locations.replace(/^, /, ''); // remove ", " from the beginning of the string
+                                eventInfo.dates = eventInfo.dates.replace(/^, /, ''); // remove ", " from the beginning of the string
+                                let titleAndArtist = capitalizeFirstLetter(eventInfo.title) + ' -' + eventInfo.lead_roles;
+                                console.log("before",titleAndArtist)
+                                if(titleAndArtist.length>58) {
+                                    titleAndArtist = titleAndArtist.slice(0,40)+'...';
+                                    console.log("after",titleAndArtist)
+                                }
+
+                                eventInfo.titleAndArtist = titleAndArtist;
+
+                                res.render('temp', { eventInfo, reviewList, showList });
+                            }
+                        })
+
+                    }
+                })
             }
         });
 
     }
-    // model.getEventReviews(eventID, (err, data) => {
-    //     if (err) {
-    //         console.log("reviews")
-    //         res.json({ error: err });
-    //     }
-    //     else {
-    //         // keep the 3 first reviews
-    //         reviewList = data.slice(0, 3);
-    //         // res.send(reviewList)
-    //     }
-    // })
-
-    // model.getShowInfo(eventID, (err, data) => {
-    //     if (err) {
-    //         console.log("show")
-
-    //         res.json({ error: err });
-    //     }
-    //     else {
-    //         showList = data;
-    //     }
-    // })
-
-    // model.getModalInfo(eventID, (err, data) => {
-    //     if (err) {
-    //         res.json({ error: err });
-    //     }
-    //     else {
-    //         modalInfo = data;
-    //     }
-    // })
-
-    // res.render('booking', {eventInfo, reviewList, showList, modalInfo });
 
 }
+
+function capitalizeFirstLetter(str) {
+    // Split the string into words
+    let words = str.split(' ');
+
+    // Capitalize the first letter of each word
+    for (let i = 0; i < words.length; i++) {
+        // Convert the word to lowercase
+        let lowercaseWord = words[i].toLowerCase();
+        // Capitalize the first letter and concatenate with the rest of the word
+        words[i] = lowercaseWord.charAt(0).toUpperCase() + lowercaseWord.slice(1);
+    }
+
+    // Join the words back into a string
+    return words.join(' ');
+}
+
+
 
 router.get('/:type/events/:id', bookTicketsNavigation);
 
