13bac91db945a2205c1e3cb7b31945d15a208379
Kosmas Archontis <up1084020@ac.upatras.gr>
2024-05-23 09:58:04 +0300

    Completed eventsController, changed placeholder text in search bar

--- controllers/eventsController.mjs
+++ controllers/eventsController.mjs
@@ -118,4 +118,58 @@ async function musicEvents(req, res, navigateTo) {
     });
 }
 
-export { theaterEvents, musicEvents };
+async function cinemaEvents(req, res, navigateTo) {
+    const site_header = 'CINEMA';
+    model.getAllCinema((err, data) => {
+        const cinemaEvents = {};
+        data.forEach(event => {
+            const { cinemaID: eventID, title, ...otherDetails } = event;
+            if (!cinemaEvents[eventID]) {
+                cinemaEvents[eventID] = {
+                    eventID,
+                    title,
+                    cities: [],
+                    ...otherDetails,
+                };
+            }
+            let early_date = new Date(cinemaEvents[eventID].earliest_date);
+            let formattedDate = early_date.toISOString().split('T')[0];
+            cinemaEvents[eventID].earliest_date = formattedDate;
+            let late_date = new Date(cinemaEvents[eventID].latest_date);
+            formattedDate = late_date.toISOString().split('T')[0];
+            cinemaEvents[eventID].latest_date = formattedDate;
+
+            cinemaEvents[eventID].cities.push(event.city);
+
+        });
+
+        for (let key in cinemaEvents) {
+            cinemaEvents[key].cities = cinemaEvents[key].cities.join(", ");
+
+            if (cinemaEvents[key].description.length > 190) {
+                cinemaEvents[key].description = cinemaEvents[key].description.slice(0, 190) + ' . . .';
+            }
+
+        }
+        const eventList = Object.values(cinemaEvents);
+
+        let carouselEventList = eventList.slice(0, 3).map(({ eventID, title, imageURL }) => ({ eventID, title, imageURL }));
+
+        if (carouselEventList.length < 3) {
+            for (let i = 0; i < 3 - carouselEventList.length; i++) {
+                carouselEventList[2 - i] = carouselEventList[0];
+            }
+        }
+
+        for (let i in carouselEventList) {
+            carouselEventList[i].index = i;
+            carouselEventList[i].titleU = carouselEventList[i].title.toUpperCase();
+            carouselEventList[i].type = navigateTo;
+        }
+        // res.json(eventList);
+        res.render('events', { site_header, eventList, carouselEventList });
+
+    });
+}
+
+export { theaterEvents, musicEvents, cinemaEvents };
--- routes/events.js
+++ routes/events.js
@@ -1,76 +1,30 @@
 import express from 'express';
 const router = express.Router();
 
-
-import * as model from '../model/dbInterface.js';
 import * as eventsController from '../controllers/eventsController.mjs';
-import { formatDate } from '../public/scripts/formatDate.js';
 
-let eventNavigation = async function (req, res) {
-    const navigateTo = req.params.type;
-    let site_header = 'EVENTS';
-    if (navigateTo === 'theater') {
-        eventsController.theaterEvents(req, res, navigateTo);
+let eventNavigation = async function (req, res, next) {
+    try {
+        const navigateTo = req.params.type;
+        let site_header = 'EVENTS';
+        if (navigateTo === 'theater') {
+            eventsController.theaterEvents(req, res, navigateTo);
+        }
+        else if (navigateTo === 'music') {
+            eventsController.musicEvents(req, res, navigateTo);
+        }
+        else if (navigateTo === 'cinema') {
+            eventsController.cinemaEvents(req, res, navigateTo);
+        }
+        else {
+            res.json({ error: 'Invalid navigation' });
+        }
     }
-    else if (navigateTo === 'music') {
-        eventsController.musicEvents(req, res, navigateTo);
-    }
-    else if (navigateTo === 'cinema') {
-        site_header = 'CINEMA';
-        model.getAllCinema((err, data) => {
-            const cinemaEvents = {};
-            data.forEach(event => {
-                const { cinemaID: eventID, title, ...otherDetails } = event;
-                if (!cinemaEvents[eventID]) {
-                    cinemaEvents[eventID] = {
-                        eventID,
-                        title,
-                        cities: [],
-                        ...otherDetails,
-                    };
-                }
-                let early_date = new Date(cinemaEvents[eventID].earliest_date);
-                let formattedDate = early_date.toISOString().split('T')[0];
-                cinemaEvents[eventID].earliest_date = formattedDate;
-                let late_date = new Date(cinemaEvents[eventID].latest_date);
-                formattedDate = late_date.toISOString().split('T')[0];
-                cinemaEvents[eventID].latest_date = formattedDate;
-
-                cinemaEvents[eventID].cities.push(event.city);
-
-            });
-
-            for (let key in cinemaEvents) {
-                cinemaEvents[key].cities = cinemaEvents[key].cities.join(", ");
-
-                if (cinemaEvents[key].description.length > 190) {
-                    cinemaEvents[key].description = cinemaEvents[key].description.slice(0, 190) + ' . . .';
-                }
-
-            }
-            const eventList = Object.values(cinemaEvents);
-
-            let carouselEventList = eventList.slice(0, 3).map(({ eventID, title, imageURL }) => ({ eventID, title, imageURL }));
-
-            if (carouselEventList.length < 3) {
-                for (let i = 0; i < 3 - carouselEventList.length; i++) {
-                    carouselEventList[2 - i] = carouselEventList[0];
-                }
-            }
-
-            for (let i in carouselEventList) {
-                carouselEventList[i].index = i;
-                carouselEventList[i].titleU = carouselEventList[i].title.toUpperCase();
-                carouselEventList[i].type = navigateTo;
-            }
-            // res.json(eventList);
-            res.render('events', { site_header, eventList, carouselEventList });
-
-        });
-    }
-    else {
-        // raise Error
-        res.json({ error: 'Invalid navigation' });
+    catch (error) {
+        // when error is thrown, the next middleware is called
+        // next(new Error('Invalid navigation'));
+        console.error(error);
+        res.json({ error: 'An error occurred' });
     }
 };
 
