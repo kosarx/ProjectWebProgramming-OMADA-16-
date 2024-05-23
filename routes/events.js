c9bfcb728c4edec8c268384a3670f16e54f16434
Kosmas Archontis <up1084020@ac.upatras.gr>
2024-05-23 07:46:34 +0300

    TODO: Moved controller logic to ./controllers, did theaterEvents and musicEvents

--- routes/events.js
+++ routes/events.js
@@ -1,126 +1,19 @@
 import express from 'express';
 const router = express.Router();
 
+
 import * as model from '../model/dbInterface.js';
+import * as eventsController from '../controllers/eventsController.mjs';
 import { formatDate } from '../public/scripts/formatDate.js';
 
 let eventNavigation = async function (req, res) {
     const navigateTo = req.params.type;
     let site_header = 'EVENTS';
     if (navigateTo === 'theater') {
-        site_header = 'THEATER';
-        model.getAllTheater((err, data) => {
-            const theaterEvents = {};
-            data.forEach(event => {
-                const { theaterID: eventID, title, ...otherDetails } = event;
-                if (!theaterEvents[eventID]) {
-                    theaterEvents[eventID] = {
-                        eventID,
-                        title,
-                        cities: [],
-                        earliestEventDate: '',
-                        latestEventDate: '',
-                        ...otherDetails,
-                    };
-                }
-                if (theaterEvents[eventID].earliestEventDate === "") {
-                    
-                    const { dayName, formattedDate } = formatDate(theaterEvents[eventID].earliest_date);
-                                    
-                    theaterEvents[eventID].earliestEventDate = formattedDate;
-                    
-
-                }
-                let early_date = new Date(theaterEvents[eventID].earliest_date);
-                let formattedDate = early_date.toISOString().split('T')[0];
-                theaterEvents[eventID].earliest_date = formattedDate;
-                let late_date = new Date(theaterEvents[eventID].latest_date);
-                formattedDate = late_date.toISOString().split('T')[0];
-                theaterEvents[eventID].latest_date = formattedDate;
-                const { dayName, formattedDateLatest } = formatDate(theaterEvents[eventID].latest_date);
-                theaterEvents[eventID].latestEventDate = formattedDateLatest;
-                
-
-
-                theaterEvents[eventID].cities.push(event.city);
-            });
-
-            for (let key in theaterEvents) {
-                theaterEvents[key].cities = theaterEvents[key].cities.join(", ");
-                if (theaterEvents[key].description.length > 190) {
-                    theaterEvents[key].description = theaterEvents[key].description.slice(0, 190) + ' . . .';
-                }
-            }
-            const eventList = Object.values(theaterEvents);
-            let carouselEventList = eventList.slice(0, 3).map(({ eventID, title, imageURL }) => ({ eventID, title, imageURL }));
-
-            if (carouselEventList.length < 3) {
-                for (let i = 0; i < 3 - carouselEventList.length; i++) {
-                    carouselEventList[2 - i] = carouselEventList[0];
-                }
-            }
-
-
-            for (let i in carouselEventList) {
-                carouselEventList[i].index = i;
-                carouselEventList[i].titleU = carouselEventList[i].title.toUpperCase();
-                carouselEventList[i].type = navigateTo;
-            }
-            // res.json(eventList);
-            res.render('events', { site_header, eventList, carouselEventList });
-        });
+        eventsController.theaterEvents(req, res, navigateTo);
     }
     else if (navigateTo === 'music') {
-        site_header = 'MUSIC';
-        model.getAllMusic((err, data) => {
-            const musicEvents = {};
-            data.forEach(event => {
-                const { musicID: eventID, title, ...otherDetails } = event;
-                if (!musicEvents[eventID]) {
-                    musicEvents[eventID] = {
-                        eventID,
-                        title,
-                        cities: [],
-                        ...otherDetails,
-                    };
-                }
-                let early_date = new Date(musicEvents[eventID].earliest_date);
-                
-                let formattedDate = early_date.toISOString().split('T')[0];
-                musicEvents[eventID].earliest_date = formattedDate;
-                let late_date = new Date(musicEvents[eventID].latest_date);
-                formattedDate = late_date.toISOString().split('T')[0];
-                musicEvents[eventID].latest_date = formattedDate;
-
-                musicEvents[eventID].cities.push(event.city);
-            });
-
-            for (let key in musicEvents) {
-                musicEvents[key].cities = musicEvents[key].cities.join(", ");
-
-                if (musicEvents[key].description.length > 190) {
-                    musicEvents[key].description = musicEvents[key].description.slice(0, 190) + ' . . .';
-                }
-            }
-            const eventList = Object.values(musicEvents);
-            let carouselEventList = eventList.slice(0, 3).map(({ eventID, title, imageURL }) => ({ eventID, title, imageURL }));
-
-            if (carouselEventList.length < 3) {
-                for (let i = 0; i < 3 - carouselEventList.length; i++) {
-                    carouselEventList[2 - i] = carouselEventList[0];
-                }
-            }
-
-
-            for (let i in carouselEventList) {
-                carouselEventList[i].index = i;
-                carouselEventList[i].titleU = carouselEventList[i].title.toUpperCase();
-                carouselEventList[i].type = navigateTo;
-            }
-            // res.json(eventList);
-            res.render('events', { site_header, eventList, carouselEventList });
-
-        });
+        eventsController.musicEvents(req, res, navigateTo);
     }
     else if (navigateTo === 'cinema') {
         site_header = 'CINEMA';
