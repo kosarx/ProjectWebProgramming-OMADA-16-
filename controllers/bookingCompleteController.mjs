aee73e4bee61bfd3264bb95a0138b496a1dddfba
Kosmas Archontis <up1084020@ac.upatras.gr>
2024-05-26 10:43:02 +0300

    persistent infinite ticket hack wiped out

--- controllers/bookingCompleteController.mjs
+++ controllers/bookingCompleteController.mjs
@@ -51,121 +51,123 @@ async function getBookingComplete(req, res, next) {
         console.error(error_comment);
         next(new Error(error_comment));
     }
-    // get show details
-    model.getES_E_V(showID, async (err, result) => {
-        if (err) {
-            const error_comment = "Failed to get show details";
-            req.session.error_comment = error_comment;
-            console.error(error_comment);
-            next(err);
-        }
-        else {
-            let eventShowInfo = result[0];
-            eventShowInfo.show_date = formatDate(eventShowInfo.show_date).formattedDate;
-            
-            // calculate total price
-            let totalAmount = 0;
-            tickets.forEach(ticket => {
-                ticket.finalPrice = Number(ticket.finalPrice.split('€')[0]).toFixed(2);
-                totalAmount += Number(ticket.finalPrice);
-
-                // generate a 7-digit random number for the ticket number:
-                const ticketNumber = Math.floor(1000000 + Math.random() * 9000000);
-                ticket.ticketNumber = ticketNumber;
-
-                // rename key category to seatingCategory and discount to discountCategory
-                ticket.seatingCategory = ticket.category;
-                ticket.discountCategory = ticket.discount;
-            });
-            totalAmount = totalAmount.toFixed(2);
-
-            const success = processPayment(req, res, next); // dummy function
-            let bookingInfo;
-            if (success) {
-                // store the tickets in the database
-                for (const ticket of tickets) {
-                    try {
-                        console.log("Storing ticket in the database");
-            
-                        // get the discountID from the discount category
-                        let discountCategoryName = ticket.discountCategory;
-                        if (ticket.discountCategory !== "Regular") {
-                            discountCategoryName = ticket.discountCategory.split(',')[0];
-                        }
-                        const discountCategory = await model.getDiscountFromType(discountCategoryName);
-                        const discountID = discountCategory[0].discountID;
-            
-                        // get the categoryID from the seating category
-                        const seatCategory = await model.getSeatCategoryFromName(ticket.seatingCategory);
-                        const categoryID = seatCategory[0].categoryID;
-            
-                        // get current timestamp
-                        const date_booked = new Date().toISOString().slice(0, 19).replace('T', ' ');
-                        // console.log("discountID", discountID, "categoryID", categoryID, "showID", showID, "date_booked", date_booked, "userID", req.session.loggedUserId);
-                        
-                        const lastInsertedTicketID = await model.insertTicket(ticket.ticketNumber, 'BOOKED', categoryID, req.session.loggedUserId, date_booked, discountID, showID);
-                        // destroy the booking token
-                        ticket.ticketID = lastInsertedTicketID;
-                        console.log("Ticket stored in the database", ticket.ticketID);
-                    } catch (err) {
-                        const error_comment = "Failed to store ticket in the database";
-                        console.error(error_comment);
-                        next(err);
-                    }
-                }
-
-                eventShowInfo = {
-                    success: success,
-                    eventID: eventShowInfo.eventID,
-                    imageURL: eventShowInfo.imageURL,
-                    title: eventShowInfo.title,
-                    showDate: eventShowInfo.show_date,
-                    showTime: eventShowInfo.show_time,
-                    venueName: eventShowInfo.venue_name,
-                    venueCity: eventShowInfo.city,
-                    venueAddress: eventShowInfo.address,
-                    description: eventShowInfo.description,
-                };
+    else {
+        // get show details
+        model.getES_E_V(showID, async (err, result) => {
+            if (err) {
+                const error_comment = "Failed to get show details";
+                req.session.error_comment = error_comment;
+                console.error(error_comment);
+                next(err);
+            }
+            else {
+                let eventShowInfo = result[0];
+                eventShowInfo.show_date = formatDate(eventShowInfo.show_date).formattedDate;
                 
-                bookingInfo = tickets.map(ticket => ({
-                    ticketID: ticket.ticketID,
-                    userID: req.session.loggedUserId,
-                    ticketNumber: ticket.ticketNumber,
-                    seatingCategory: ticket.seatingCategory,
-                    discountCategory: ticket.discountCategory,
-                    finalPrice: ticket.finalPrice,
-                    downloadLink: ticket.downloadLink,
-                    ...eventShowInfo,
-                }));
+                // calculate total price
+                let totalAmount = 0;
+                tickets.forEach(ticket => {
+                    ticket.finalPrice = Number(ticket.finalPrice.split('€')[0]).toFixed(2);
+                    totalAmount += Number(ticket.finalPrice);
 
-                bookingInfo.forEach(ticket => {
-                    // check if ticket.discountCategory contains '%' Causes problems with URL encoding
-                    if (ticket.discountCategory.includes('%')) {
-                        // change it to '%25'
-                        ticket.discountCategory = ticket.discountCategory.replace('%', '%25');
-                    }
-                    // URL enconde the bookingInfo object
-                    const bookingInfoEncoded = encodeURIComponent(JSON.stringify(ticket));
-                    // create a download link for the tickets
-                    const downloadLink = `/booking/complete/download?bookingInfo=${bookingInfoEncoded}`;
-                    ticket.downloadLink = downloadLink;
+                    // generate a 7-digit random number for the ticket number:
+                    const ticketNumber = Math.floor(1000000 + Math.random() * 9000000);
+                    ticket.ticketNumber = ticketNumber;
 
-                    // revert the change to '%'
-                    if (ticket.discountCategory.includes('%25')) {
-                        ticket.discountCategory = ticket.discountCategory.replace('%25', '%');
-                    }
+                    // rename key category to seatingCategory and discount to discountCategory
+                    ticket.seatingCategory = ticket.category;
+                    ticket.discountCategory = ticket.discount;
                 });
+                totalAmount = totalAmount.toFixed(2);
+
+                const success = processPayment(req, res, next); // dummy function
+                let bookingInfo;
+                if (success) {
+                    // store the tickets in the database
+                    for (const ticket of tickets) {
+                        try {
+                            console.log("Storing ticket in the database");
+                
+                            // get the discountID from the discount category
+                            let discountCategoryName = ticket.discountCategory;
+                            if (ticket.discountCategory !== "Regular") {
+                                discountCategoryName = ticket.discountCategory.split(',')[0];
+                            }
+                            const discountCategory = await model.getDiscountFromType(discountCategoryName);
+                            const discountID = discountCategory[0].discountID;
+                
+                            // get the categoryID from the seating category
+                            const seatCategory = await model.getSeatCategoryFromName(ticket.seatingCategory);
+                            const categoryID = seatCategory[0].categoryID;
+                
+                            // get current timestamp
+                            const date_booked = new Date().toISOString().slice(0, 19).replace('T', ' ');
+                            // console.log("discountID", discountID, "categoryID", categoryID, "showID", showID, "date_booked", date_booked, "userID", req.session.loggedUserId);
+                            
+                            const lastInsertedTicketID = await model.insertTicket(ticket.ticketNumber, 'BOOKED', categoryID, req.session.loggedUserId, date_booked, discountID, showID);
+                            // destroy the booking token
+                            ticket.ticketID = lastInsertedTicketID;
+                            console.log("Ticket stored in the database", ticket.ticketID);
+                        } catch (err) {
+                            const error_comment = "Failed to store ticket in the database";
+                            console.error(error_comment);
+                            next(err);
+                        }
+                    }
+
+                    eventShowInfo = {
+                        success: success,
+                        eventID: eventShowInfo.eventID,
+                        imageURL: eventShowInfo.imageURL,
+                        title: eventShowInfo.title,
+                        showDate: eventShowInfo.show_date,
+                        showTime: eventShowInfo.show_time,
+                        venueName: eventShowInfo.venue_name,
+                        venueCity: eventShowInfo.city,
+                        venueAddress: eventShowInfo.address,
+                        description: eventShowInfo.description,
+                    };
+                    
+                    bookingInfo = tickets.map(ticket => ({
+                        ticketID: ticket.ticketID,
+                        userID: req.session.loggedUserId,
+                        ticketNumber: ticket.ticketNumber,
+                        seatingCategory: ticket.seatingCategory,
+                        discountCategory: ticket.discountCategory,
+                        finalPrice: ticket.finalPrice,
+                        downloadLink: ticket.downloadLink,
+                        ...eventShowInfo,
+                    }));
+
+                    bookingInfo.forEach(ticket => {
+                        // check if ticket.discountCategory contains '%' Causes problems with URL encoding
+                        if (ticket.discountCategory.includes('%')) {
+                            // change it to '%25'
+                            ticket.discountCategory = ticket.discountCategory.replace('%', '%25');
+                        }
+                        // URL enconde the bookingInfo object
+                        const bookingInfoEncoded = encodeURIComponent(JSON.stringify(ticket));
+                        // create a download link for the tickets
+                        const downloadLink = `/booking/complete/download?bookingInfo=${bookingInfoEncoded}`;
+                        ticket.downloadLink = downloadLink;
+
+                        // revert the change to '%'
+                        if (ticket.discountCategory.includes('%25')) {
+                            ticket.discountCategory = ticket.discountCategory.replace('%25', '%');
+                        }
+                    });
+                }
+                let error_comment;
+                let error_message;
+                if (!success) {
+                    error_comment = "Network error while processing payment."; // generic dummy comment
+                    error_message = "Payment processing failed. Please try again."; // generic dummy message
+                }
+                req.session.bookingToken = "Token used";
+                res.render('booking_complete', {success: bookingInfo[0].success, eventInfo: eventShowInfo, tickets: bookingInfo, totalAmount, error_comment, error_message});
             }
-            let error_comment;
-            let error_message;
-            if (!success) {
-                error_comment = "Network error while processing payment."; // generic dummy comment
-                error_message = "Payment processing failed. Please try again."; // generic dummy message
-            }
-            req.session.bookingToken = "Token used";
-            res.render('booking_complete', {success: bookingInfo[0].success, eventInfo: eventShowInfo, tickets: bookingInfo, totalAmount, error_comment, error_message});
-        }
-    });
+        });
+    }
 }
 
 // Function to generate a PDF document
